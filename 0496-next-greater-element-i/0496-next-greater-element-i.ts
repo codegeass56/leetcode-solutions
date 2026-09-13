d7aec96297eb941110b594447351e3346b1ea6ae
Siddharth Panjwani <siddharthkpanjwani@gmail.com>
2026-09-13 19:09:04 +0400

    Time: 2 ms (90.05%), Space: 58.9 MB (37.9%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.ts
+++ 0496-next-greater-element-i/0496-next-greater-element-i.ts
@@ -5,7 +5,7 @@ function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
         while(stack.length > 0 && stack[stack.length - 1] <= nums2[i]) {
             stack.pop();
         }
-        map.set(nums2[i], stack.length > 0 && stack[stack.length - 1] > nums2[i] ? stack[stack.length - 1] : -1);
+        map.set(nums2[i], stack.length > 0 ? stack[stack.length - 1] : -1);
         stack.push(nums2[i]);
     }
 
